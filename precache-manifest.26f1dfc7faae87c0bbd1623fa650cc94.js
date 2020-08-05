5ef2a7058b7f4779c88fd78e4344a9b457d6d2c2
hoshino88 <shingo.hoshino88@gmail.com>
2020-08-05 09:17:10 +0900

    Updates

---
+++ precache-manifest.26f1dfc7faae87c0bbd1623fa650cc94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05b3258f7b4f1ea575bd3cee77148a1c",
+    "url": "/coat-app-dnd/index.html"
+  },
+  {
+    "revision": "e3ffb0710d3a53425fa7",
+    "url": "/coat-app-dnd/static/js/2.cdafaa64.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/coat-app-dnd/static/js/2.cdafaa64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f963cf79d5ea1dab9795",
+    "url": "/coat-app-dnd/static/js/main.aff0b529.chunk.js"
+  },
+  {
+    "revision": "8270c4b8d4b2d9654fcb",
+    "url": "/coat-app-dnd/static/js/runtime-main.d79565b5.js"
+  }
+]);
