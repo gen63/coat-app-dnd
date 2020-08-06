b59905b8395507c2cfa9941c62eb005eca4cbb8f
hoshino88 <shingo.hoshino88@gmail.com>
2020-08-07 08:23:21 +0900

    Updates

--- precache-manifest.6db0434c550d5e046ccdb02d5efe6a5a.js
+++ precache-manifest.6db0434c550d5e046ccdb02d5efe6a5a.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a8307495a1a58dcd9f8d6a21ec617244",
+    "revision": "98f14a227030c43084ecfceb9cec4f7a",
     "url": "/coat-app-dnd/index.html"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/coat-app-dnd/static/js/2.ded2ecc9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2ffb80aca0aae1930ab3",
-    "url": "/coat-app-dnd/static/js/main.42936b07.chunk.js"
+    "revision": "c882d9e157fffe0a3d46",
+    "url": "/coat-app-dnd/static/js/main.92bdfbdd.chunk.js"
   },
   {
     "revision": "8270c4b8d4b2d9654fcb",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/coat-app-dnd/precache-manifest.d11594569f1cd855de1a0587bbe12989.js"
+  "/coat-app-dnd/precache-manifest.6db0434c550d5e046ccdb02d5efe6a5a.js"
 );
 
 self.addEventListener('message', (event) => {
